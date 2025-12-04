5ebf3086f768790146c701a3f98ab23f7bad952c
VirginiaPat <virginia.patrika@gmail.com>
2025-12-04 13:57:49 +0100

    index.html & script.js updated

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const form = document.getElementById("form");
-const email = document.getElementById("email");
+const emailInput = document.getElementById("email");
 const errorMessage = document.getElementById("email-error");
 const imgMobile = document.getElementById("modile-img-ill");
 const mainContainer = document.getElementById("main-container");
@@ -10,74 +10,84 @@ const dismissBtn = document.getElementById("dismiss-btn");
 const emailConfirmed = document.getElementById("email-confirmed");
 
 // Functions
+// Show error text message
 const showError = function () {
-  if (email.validity.valueMissing) {
+  if (emailInput.validity.valueMissing) {
     // If empty
     errorMessage.textContent = "You need to enter an email address.";
-  } else if (email.validity.typeMismatch) {
+  } else if (emailInput.validity.typeMismatch || !emailInput.validity.valid) {
     // If it's not an email address
     errorMessage.textContent = "Valid email required";
   }
-  errorMessage.classList.remove("hidden");
+  errorMessage.classList.remove("sr-only");
 };
 
-// Event listeners
-email.addEventListener("input", function (e) {
-  // if email valid
-  if (email.validity.valid) {
-    errorMessage.classList.add("hidden"); //hide the error message
-    email.classList.remove("input-style--error"); //remove red background when it exists
-  } else {
-    showError();
-  }
-});
+// Show pop-up function
+const showSuccessPopup = function () {
+  imgMobile.classList.add("hidden");
+  mainContainer.classList.remove("flex,flex-col");
+  mainContainer.classList.add("hidden");
+  emailInput.classList.remove("input-style--error");
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
+  // show success pop-up
+  successPopup.classList.remove("hidden");
+  successPopup.setAttribute("aria-hidden", "false");
+  emailConfirmed.textContent = emailInput.value; //add the email from the input into the pop-up text
 
-  // if the email field is invalid
-  if (!email.validity.valid) {
-    showError();
-    email.classList.add("input-style--error");
-  }
+  // clear input field
+  emailInput.value = "";
+};
 
-  // if the email field is valid
-  if (email.validity.valid) {
-    imgMobile.classList.add("hidden");
-    mainContainer.classList.remove("flex,flex-col");
-    mainContainer.classList.add("hidden");
-    email.classList.remove("input-style--error");
-
-    // show success pop-up
-    successPopup.classList.remove("hidden");
-    emailConfirmed.textContent = email.value; //add the email from the input into the text
-
-    // clear input field
-    email.value = "";
-  }
-});
-
-// function for closeing success pop-up
-const closeSuccessPopup = function () {
+// Hide success pop-up
+const hideSuccessPopup = function () {
   successPopup.classList.add("hidden");
+  successPopup.setAttribute("aria-hidden", "true");
   imgMobile.classList.remove("hidden");
   mainContainer.classList.remove("hidden");
   mainContainer.classList.add("flex,flex-col");
 };
 
+// Event listeners
+// check input's valid
+emailInput.addEventListener("input", function (e) {
+  // if email valid
+  if (emailInput.validity.valid) {
+    errorMessage.classList.add("sr-only"); // Hide visually but keep accessible
+    emailInput.classList.remove("input-style--error"); //remove red background when it exists
+  } else {
+    showError();
+  }
+});
+
+// Submiting the form when press the button
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // if the email field is invalid
+  if (!emailInput.validity.valid) {
+    showError();
+    emailInput.classList.add("input-style--error");
+  }
+
+  // if the email field is valid
+  if (emailInput.validity.valid) {
+    showSuccessPopup();
+  }
+});
+
 // close success pop-up when pressing dismiss button
 dismissBtn.addEventListener("click", function () {
-  closeSuccessPopup();
+  hideSuccessPopup();
 });
 
 // close sucess pop-up when clicking outside
 document.addEventListener("click", function (e) {
-  closeSuccessPopup();
+  hideSuccessPopup();
 });
 
 // close sucess pop-up when pressing ESC key
 document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
-    closeSuccessPopup();
+    hideSuccessPopup();
   }
 });
